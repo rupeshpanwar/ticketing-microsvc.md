6889b6de9286c43c721c768a9f3574fef210f082
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-22 15:16:31 +0530

    initial landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,12 @@
-const Landing = () => {
+const LandingPage = ({ color}) => {
+  console.log('I am in the component', color);
   return <h1>Landing Page</h1>;
 };
- 
-export default Landing;
+
+LandingPage.getInitialPage = () => {
+
+  console.log(`I'm on server`);
+  return { color: 'red'}
+}
+
+export default LandingPage;
