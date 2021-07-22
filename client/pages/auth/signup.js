e0eea725e2a5bdf8762f93f0f6122fca47dd27c3
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-22 12:46:37 +0530

    Signup-page EventHandler

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import { useState } from 'react';
 
-const signup = () => {
-    return (
-        <form>
-            <h1>Signup</h1>
-            <div className="form-group">
-                <label>Email Address</label>
-                <input className="form-control" />
-            </div>
-            <div className="form-group">
-                <label>Password</label>
-                <input type='password' className="form-control" />
-            </div>
-            <button className="btn-primary">Sign Up</button>
-        </form>
-    )
-}
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default signup
+  const onSubmit = event => {
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
