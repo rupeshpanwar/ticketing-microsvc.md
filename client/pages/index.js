19b9d4215482de30ddd15d4a8e8c7bb00af3fe65
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-22 17:26:02 +0530

    Server- Browerside - rendering- cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,32 @@
-const LandingPage = ({ color}) => {
-  console.log('I am in the component', color);
+import axios from "axios";
+
+const LandingPage = ({ currentUser}) => {
+  console.log(currentUser);
+
+  //axios.get('/api/users/currentuser')
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialPage = () => {
+LandingPage.getInitialPage = async () => {
+  if( typeof window === "undefined"){
+    // we are on server
+    // request will be made to SVC.NS.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+      )
+  } else {
+    // we are on browser
+    // request will be made to api
+    const { data } = await axios.get(
+      '/api/users/currentuser/api/users/currentuser',
+     { headers: {
+        Host: 'ticketing.dev'
+      }
+    }
+      )
+    return data
+  }
 
-  console.log(`I'm on server`);
-  return { color: 'red'}
 }
 
 export default LandingPage;
