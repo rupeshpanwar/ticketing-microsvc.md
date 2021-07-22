923d1b87325d51e8b94b64f8ab0027549e42b62b
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-22 14:57:51 +0530

    code refactor-Error&axioscall, page rendering

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,17 +1,24 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password
+    },
+    onSuccess: () => Router.push('/')
+  });
 
   const onSubmit = async event => {
     event.preventDefault();
 
-   const response = await axios.post('/api/users/signup',
-                    {email, password} 
-                    )  
-    console.log(response.data) 
+    await doRequest();
   };
 
   return (
@@ -34,6 +41,7 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
